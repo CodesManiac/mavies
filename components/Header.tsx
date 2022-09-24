4e8c19007124a0bd3e1e623bf5c0a7fc06c139cd
CodesManiac <joyajiboye2016@gmail.com>
2022-09-24 23:21:38 +0100

    add part of header

---
+++ components/Header.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+import { Notification, SearchNormal1, User } from 'iconsax-react';
+
+const Header = () => {
+  return (
+    <header className='container mx-auto p-6'>
+      <div className='flex justify-between items-center '>
+        <h1 className='bg-clip-text font-bold text-xl text-transparent bg-gradient-to-r from-[#ea4996] to-[#8d56f4]'>
+          Mavies
+        </h1>
+        <div className='hidden md:flex md:gap-5 text-white'>
+          {[
+            ['Home', '/'],
+            ['Movies', '/'],
+            ['Tv Shows', '/'],
+            ['New & Popular', '/'],
+            ['My List', '/'],
+          ].map(([title, url]) => (
+            <a
+              key={url}
+              href={url}
+              className='py-2 px-4 rounded-lg font-semibold hover:-translate-y-1 hover:scale-110 hover:bg-dark duration-300'
+            >
+              {title}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className='flex items-center space-x-4 text-sm font-light'>
+        <SearchNormal1
+          color='#ffffff'
+          className='sm hidden h-6 w-6 sm:inline'
+        />
+        <p className='hidden lg:inline'>Kids</p>
+        <Notification color='#ffffff' className='h-6 w-6 cursor-pointer' />
+        <Link href='/account'>
+          <User color='#ffffff' className=' rounded cursor-pointer' />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+    <div className=''>
       <Head>
         <title>Mavies</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       {/* header */}
+      <Header />
       <main>
         {/* banner */}
         <section>
